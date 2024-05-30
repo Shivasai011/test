44a814e5950725c4bd336b757856b21c927d275b
Auto <shivasaibogem@gmail.com>
2024-05-30 12:18:15 +0530

    main.tf2

--- main.tf
+++ main.tf
@@ -2,9 +2,9 @@ provider "aws" {
     region = "us-east-1"
 }
 
-resource "aws_instance" "ec2-1" {
-  ami = ""
-  subnet_id = ""
+resource "aws_instance" "first" {
+  ami = "ami-0e001c9271cf7f3b9"
+  subnet_id = "subnet-0100b3508f5cb942d"
   instance_type = "t3.small"
   tags = {
     Name = "first_ec2"
